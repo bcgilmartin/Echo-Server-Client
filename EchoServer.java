6e63b8242918445a985d784be53fd0d7bd490c0e
Blake <bcgilmartin@cpp.edu>
2017-04-04 15:13:31 -0700

    Implemented multiple simultaneous clients.

--- EchoServer.java
+++ EchoServer.java
@@ -11,31 +11,49 @@ import java.io.BufferedReader;
 public final class EchoServer {
 
     public static void main(String[] args) throws Exception {
+		
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-					
-					InputStream is = socket.getInputStream();
-					InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-					BufferedReader br = new BufferedReader(isr);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-					String message;
-					boolean on = true;
-					while(on) {
-						message = br.readLine();
-						if(message.equals("exit")) {
-							System.out.printf("Client disconnected: %s%n", address);
-							on = false;
-						}
-						else {
-							out.printf("Server> %s%n", message);
-						}
-					}
-                }
+					Socket socket = serverSocket.accept();
+					Runnable runner = new ParameterRun(socket);
+					new Thread(runner).start();
             }
         }
     }
+	public static class ParameterRun implements Runnable {
+
+		private Socket socket;
+	
+		public ParameterRun(Socket ThreadSocket) {
+			socket = ThreadSocket;
+		}
+
+		public void run() {
+			try {
+			
+				String address = socket.getInetAddress().getHostAddress();
+				System.out.printf("Client connected: %s%n", address);
+						
+				InputStream is = socket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+				BufferedReader br = new BufferedReader(isr);
+				OutputStream os = socket.getOutputStream();
+				PrintStream out = new PrintStream(os, true, "UTF-8");
+				String message;
+				boolean on = true;
+				while(on) {
+					message = br.readLine();
+					if(message.equals("exit")) {
+						System.out.printf("Client disconnected: %s%n", address);
+						out.printf("%n");
+						on = false;
+					}
+					else {
+						out.printf("Server> %s%n", message);
+					}
+				}
+			} catch(Exception E){E.printStackTrace();}
+			
+		}
+	}
 }
